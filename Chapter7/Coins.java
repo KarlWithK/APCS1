8c6fb18072ca2a8ab015094bbd2db6796eff66c9
Karl <jocelinc60@outlook.com>
2020-01-07 19:14:05 -0500

    Made some variables final

--- Chapter7/Coins.java
+++ Chapter7/Coins.java
@@ -1,8 +1,8 @@
 public class Coins {
-	public static void main (String[] args) {
-		MonetaryCoin Coin1 = new MonetaryCoin(25);
-		MonetaryCoin Coin2 = new MonetaryCoin(50);
-		MonetaryCoin Coin3 = new MonetaryCoin();
+	public static void main (final String[] args) {
+		final MonetaryCoin Coin1 = new MonetaryCoin(25);
+		final MonetaryCoin Coin2 = new MonetaryCoin(50);
+		final MonetaryCoin Coin3 = new MonetaryCoin();
 		System.out.println();
 		System.out.println(Coin1.getValue() + Coin2.getValue());
 		System.out.println(Coin3);
