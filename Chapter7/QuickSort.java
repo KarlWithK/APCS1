4e4bc642f0f18e27823286ec7e20adcb0206ba3f
Karl <jocelinc60@outlook.com>
2020-02-04 12:37:33 -0500

    quickSort

--- Chapter7/QuickSort.java
+++ Chapter7/QuickSort.java
@@ -1,7 +1,8 @@
 public class QuickSort {
 	public static void main(String[] args) {
-		int[] list = {5, 10, 1, 7, 8};	
-		quickSort(list, 0, list.length - 1);
+		// int[] list = {5, 10, 1, 7, 8};	
+		int[] list = {3, 0, 1, 8, 7, 2, 5, 4, 9, 6};
+		quickSortLo(list, 0, list.length - 1);
 		for (int num : list)
 			System.out.println(num);
 	}
@@ -28,4 +29,28 @@ public class QuickSort {
 			quickSort(arr, index + 1, high);
 		}
 	}
+
+	public static void quickSortLo(int[] arr, int low, int high) {
+		if (low < high) {
+			int pivot = arr[low];
+			int index = high;
+			int placeholder = 0;
+
+			for (int j = high; j > low; j--) {
+				if (arr[j] > pivot) {
+					placeholder = arr[index]; 
+					arr[index] = arr[j];
+					arr[j] = placeholder;
+					index--;
+				}
+			}
+
+			placeholder = arr[index];
+			arr[index] = arr[low];
+			arr[low] = placeholder;
+			quickSortLo(arr, low, index - 1);
+			quickSortLo(arr, index + 1, high);
+		}
+	}
+	
 }
